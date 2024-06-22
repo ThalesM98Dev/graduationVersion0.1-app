70ec3618bf86bf9916dbe1a5979d8832bbe5d94e
Thales Mustafa <thales.98mustafa@gmail.com>
2024-06-22 19:06:27 +0300

    fixes

--- routes/console.php
+++ routes/console.php
@@ -9,6 +9,6 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote')->hourly();
 
 
-Schedule::command('app:trip-date-command')->daily()->withoutOverlapping();
+//Schedule::command('app:trip-date-command')->daily()->withoutOverlapping();
 //Schedule::command('app:trip-date-command')->everyFiveSeconds();
 
