7713678f5a65a4fa35ce83609acec518a45850d6
Thales Mustafa <thales.98mustafa@gmail.com>
2024-06-25 19:42:18 +0300

    fixes

--- routes/api.php
+++ routes/api.php
@@ -132,11 +132,11 @@ Route::prefix('collage_trips')->group(function () {
 
 
 Route::prefix('feedback')->group(function () {
-    Route::get('/all', [FeedbackController::class, 'index'])->middleware('admin');
-    Route::get('/user', [FeedbackController::class, 'userFeedbacks'])->middleware('user');
+    Route::get('/all', [FeedbackController::class, 'index'])->middleware('role:admin');
+    Route::get('/user', [FeedbackController::class, 'userFeedbacks'])->middleware('role:user');
     Route::get('/show/{id}', [FeedbackController::class, 'show'])->middleware('role:Admin,User'); //Role:Admin,User,Driver,Shipment Employee,Travel Trips Employee,University trips Employee
-    Route::post('/create', [FeedbackController::class, 'store'])->middleware('user');
-    Route::delete('/delete/{id}', [FeedbackController::class, 'destroy'])->middleware('admin');
+    Route::post('/create', [FeedbackController::class, 'store'])->middleware('role:user');
+    Route::delete('/delete/{id}', [FeedbackController::class, 'destroy'])->middleware('role:admin');
 });
 
 Route::get('/days', function () {
