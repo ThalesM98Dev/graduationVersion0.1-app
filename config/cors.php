33e84ca4410b739dc0f8c06b529d923080f384ae
Tabarak Awad <tbarkawad99@gmail.com>
2024-08-11 17:54:30 +0300

    edites

--- config/cors.php
+++ config/cors.php
@@ -4,6 +4,7 @@ return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['*'],
+    'Access-Control-Allow_Origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['Authorization', '*'],
     'exposed_headers' => [],
