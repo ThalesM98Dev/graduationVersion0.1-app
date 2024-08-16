b3258e9176111cecddfc1373855046874171377d
Thales Mustafa <thales.98mustafa@gmail.com>
2024-08-16 12:47:55 +0300

    recover account

--- app/Enum/MessagesEnum.php
+++ app/Enum/MessagesEnum.php
@@ -5,7 +5,7 @@ namespace App\Enum;
 
 enum MessagesEnum: string
 {
-    case VERIFICATION = 'إن رمز تفعيل حسابك هو : %s الرجاء عدم ارساله لأحد. شركة الاتحاد العربي للنقل البرّي.';
+    case VERIFICATION = 'إن رمز تفعيل حسابك هو : %s الرجاء عدم ارساله لأحد.\nشركة الاتحاد العربي للنقل البرّي.';
     case RECOVER_PASSWORD = 'إن رمز استعادة حسابك هو : %s الرجاء عدم ارساله لأحد.شركة الاتحاد العربي للنقل البرّي.';
 
     public function formatMessage(string $code): string
