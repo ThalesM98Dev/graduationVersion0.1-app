83d7f75549222e17e27af7153cd3a7b0de2d26ef
Thales Mustafa <thales.98mustafa@gmail.com>
2024-08-21 12:20:35 +0300

    feedback

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -13,7 +13,7 @@ class FeedbackController extends Controller
 {
     public function index()
     {
-        $result = Feedback::with('user')->all()->sortByDesc('date');
+        $result = Feedback::with('user')->get()->sortByDesc('date');
         return ResponseHelper::success($result);
     }
 
