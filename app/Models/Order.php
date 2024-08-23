2b22a999e5651ba06fc5cc736fad81f4e8bc3af0
Thales Mustafa <thales.98mustafa@gmail.com>
2024-08-23 19:13:27 +0300

    fix order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,21 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
-    protected $fillable = ['name', 'address','mobile_number','nationality', 'age', 'image_of_ID','image_of_passport','image_of_security_clearance','image_of_visa','user_id'];
+    protected $guarded = [];
 
-    public function user(){
+    public function user()
+    {
 
         return $this->belongsTo(User::class);
     }
 
-   // public function trips(){
+    // public function trips(){
 
-     //   return $this->belongsToMany(Trip::class ,'reservations' , 'order_id' , 'trip_id')->withTimestamps();
-   // }
+    //   return $this->belongsToMany(Trip::class ,'reservations' , 'order_id' , 'trip_id')->withTimestamps();
+    // }
 
     public function reservations()
     {
         return $this->belongsToMany(Reservation::class, 'reservation_orders', 'order_id', 'reservation_id')->using(ReservationOrder::class)->withTimestamps();
     }
-
 }
