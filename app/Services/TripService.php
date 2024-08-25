6fef9ffc269df2c8b2e62e0b8c66a8fefb5f2813
Thales Mustafa <thales.98mustafa@gmail.com>
2024-08-25 09:49:12 +0300

    fix subscription and reservation

--- app/Services/TripService.php
+++ app/Services/TripService.php
@@ -195,7 +195,7 @@ class TripService
                 if ($trip->available_seats == 0) {
                     return ResponseHelper::error('There is no available seats on this trip.');
                 }
-                $existReservation = $user->reservation()->where('trip_id', $trip->id)->first();
+                $existReservation = $user->dailyCollageReservations()->where('trip_id', $trip->id)->first();
                 if ($existReservation) {
                     return ResponseHelper::error('Your allready have a reservation.');
                 }
